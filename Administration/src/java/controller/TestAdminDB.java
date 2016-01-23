28b12f414c766aea34e0454a5cab0aaa57c87bda
claire meymandi <claire meymandi>
2016-01-23 17:34:14 +0100

    [Claire] Database admin tested and functionning

---
+++ Administration/src/java/controller/TestAdminDB.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.util.ArrayList;
+import model.DB;
+
+/**
+ *
+ * @author omar
+ */
+public class TestAdminDB {
+
+    public static void main(String[] args) {
+//        AdministrationWS_Service administrationWS_Service = new AdministrationWS_Service();
+//        AdministrationWS administrationWS = administrationWS_Service.getAdministrationWSPort();
+//        System.out.println(administrationWS.listAgreement());
+//        ArrayList<model.Agreement> agreements = administrationWS.listAgreement();
+//        for (int i = 0; i < agreements.size(); i++) {
+//            System.out.println(agreements.get(i).toString());
+//
+//        }
+
+        DB db = new DB();
+//        db.createPendingInternship("ghader@etud.insa-toulouse.fr", "Sogeti", "1/10/2015", 850, "DevOps", "DevOps", "Thibault Normand", "0762623226", "Security", "TLSE");
+//        db.generateAgreement("Sogeti", "ghader@etud.insa-toulouse.fr", "offre4");
+        db.acceptPendingInternship("ghader@etud.insa-toulouse.fr", "offre4");
+        db.refusePendingInternship("ghader@etud.insa-toulouse.fr", "offre4");
+
+        //List Agreements
+//        ArrayList<model.Agreement> agreements = db.listAgreement();
+//        for (int i = 0; i < agreements.size(); i++) {
+//            System.out.println(agreements.get(i).toString());
+//
+//        }
+//
+//        ArrayList<model.PendingInternship> internships = db.listPendingInternship();
+//        for (int i = 0; i < internships.size(); i++) {
+//            System.out.println(internships.get(i).toString());
+//
+//        }
+//
+//        System.out.println(db.listAgreementByStudent("mabille@etud.insa-toulouse.fr"));
+//        System.out.println(db.listPendingInternshipByStudent("mabille@etud.insa-toulouse.fr"));
+    }
+}
--- Administration/src/java/model/Agreement.java
+++ Administration/src/java/model/Agreement.java
@@ -10,77 +10,115 @@ package model;
  * @author claire
  */
 public class Agreement {
+
     private int id;
     private String idStudent;
+    private String idCompany;
     private String idPendingInternship;
     private boolean signAdmin;
     private boolean signStudent;
     private boolean signCorp;
-    
-    public Agreement(int id, String idStudent, String idPendingInternship,
-            boolean signAdmin, boolean signStudent, boolean signCorp) {
+
+    public Agreement(int id, String idStudent, String idCompany, String idPendingInternship, boolean signAdmin, boolean signStudent, boolean signCorp) {
         this.id = id;
         this.idStudent = idStudent;
+        this.idCompany = idCompany;
         this.idPendingInternship = idPendingInternship;
         this.signAdmin = signAdmin;
         this.signStudent = signStudent;
         this.signCorp = signCorp;
     }
-    
-    public int getId(){
+
+    public String getIdCompany() {
+        return idCompany;
+    }
+
+    public void setIdCompany(String idCompany) {
+        this.idCompany = idCompany;
+    }
+
+    public boolean isSignAdmin() {
+        return signAdmin;
+    }
+
+    public void setSignAdmin(boolean signAdmin) {
+        this.signAdmin = signAdmin;
+    }
+
+    public boolean isSignStudent() {
+        return signStudent;
+    }
+
+    public void setSignStudent(boolean signStudent) {
+        this.signStudent = signStudent;
+    }
+
+    public boolean isSignCorp() {
+        return signCorp;
+    }
+
+    public void setSignCorp(boolean signCorp) {
+        this.signCorp = signCorp;
+    }
+
+    public int getId() {
         return id;
     }
-    
+
     public String getIdStudent() {
         return idStudent;
     }
-    
+
     public String getIdPendingInternship() {
         return idPendingInternship;
     }
-    
+
     public boolean getSignAdmin() {
         return signAdmin;
     }
-    
+
     public boolean getSignStudent() {
         return signStudent;
     }
-    
+
     public boolean getSignCorp() {
         return signCorp;
     }
-    
-    public void setId(int id){
+
+    public void setId(int id) {
         this.id = id;
     }
-    
+
     public void setIdStudent(String idStudent) {
         this.idStudent = idStudent;
     }
-    
+
     public void getIdPendingInternship(String idPendingInternship) {
         this.idPendingInternship = idPendingInternship;
     }
-    
+
     public void getSignAdmin(boolean signAdmin) {
         this.signAdmin = signAdmin;
     }
-    
+
     public void getSignStudent(boolean signStudent) {
         this.signStudent = signStudent;
     }
-    
+
     public void getSignCorp(boolean signCorp) {
         this.signCorp = signCorp;
     }
-    
+
     public String toString() {
         StringBuilder res = new StringBuilder();
         res.append("{'id':");
         res.append(id);
         res.append(",'idStudent':");
         res.append(idStudent);
+
+        res.append(",'idCompany':");
+        res.append(idCompany);
+
         res.append(",'idPendingInternship':");
         res.append(idPendingInternship);
         res.append(",'signAdmin':");
@@ -88,7 +126,7 @@ public class Agreement {
         res.append(",'signStudent':");
         res.append(signStudent);
         res.append(",'signCorp':");
-        res.append(signCorp);        
+        res.append(signCorp);
         return res.toString();
     }
 }
